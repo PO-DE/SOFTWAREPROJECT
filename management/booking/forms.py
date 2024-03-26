1184348c662ec2dd4923497754c086b32fa46cde
Raj Modi <ra_modi@live.concordia.ca>
2024-03-25 23:34:35 -0400

    booking model updated

--- management/booking/forms.py
+++ management/booking/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from django.db.models import Q
 from management.booking.models import Booking
-from django.contrib.auth.models import User, Group
+from management.package.models import Package
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
@@ -16,10 +15,3 @@ class BookingForm(forms.ModelForm):
         super(BookingForm, self).__init__(*args, **kwargs)
         if package is not None:
             self.fields['Package'].initial = package
-        agent_group = Group.objects.filter(name="Agent").first()
-        if agent_group:
-            self.fields['user'].queryset = User.objects.filter(
-                Q(groups=agent_group) | Q(is_staff=True)
-            )
-        else:
-            self.fields['user'].queryset = User.objects.filter(is_staff=True)
