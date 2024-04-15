eac7073408e75fd7b1bd111775b582f9d7b753bc
Raj Modi <ra_modi@live.concordia.ca>
2024-04-15 16:13:30 -0400

    details updated

--- management/views.py
+++ management/views.py
@@ -3,16 +3,16 @@ from management.package.models import Package
 
 def index(request):
     try:
-        package_alberta = Package.objects.get(pk=5)  # Replace with the actual ID
-        package_toronto = Package.objects.get(pk=4)  # Replace with the actual ID
-        package_mumbai = Package.objects.get(pk=7)  # Replace with the actual ID
+        package_alberta = Package.objects.get(pk=1)  # Replace with the actual ID
+        package_newyork = Package.objects.get(pk=3)  # Replace with the actual ID
+        package_mumbai = Package.objects.get(pk=2)  # Replace with the actual ID
     except Package.DoesNotExist:
         package_alberta = package_toronto = package_mumbai = None
 
         # Pass them to the context
     context = {
         'package_alberta': package_alberta,
-        'package_toronto': package_toronto,
+        'package_newyork': package_newyork,
         'package_mumbai': package_mumbai,
     }
 
